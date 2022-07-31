9e93ea2340bbc672580a26b2fb27733be22e8f35
taofiqSulayman <taofiqsulayman@gmail.com>
2022-07-31 18:41:01 +0100

    Test passed with obvious implementation

--- src/main/java/com/tao/LeapYearChecker.java
+++ src/main/java/com/tao/LeapYearChecker.java
@@ -2,7 +2,8 @@ package com.tao;
 
 public class LeapYearChecker {
     public static boolean isLeapYear(int year) {
-        if (year % 100 == 0)
+
+        if (year % 100 == 0 && year % 400 != 0)
             return false;
 
         if (year % 4 == 0)
