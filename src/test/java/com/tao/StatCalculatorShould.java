2ee9d876446e0a9fdb9d1aa065f0b8e84e7233a1
taofiqSulayman <taofiqsulayman@gmail.com>
2022-07-31 19:54:49 +0100

    second failing test

--- src/test/java/com/tao/StatCalculatorShould.java
+++ src/test/java/com/tao/StatCalculatorShould.java
@@ -13,4 +13,13 @@ public class StatCalculatorShould {
         int minValue = StatCalculator.getMinValue(arr);
         assertEquals(-2, minValue);
     }
+
+    @Test
+    public void giveMinValueOfArray2(){
+
+        int[] arr = {6, 9, 15, -3, 92, 11};
+        int minValue = StatCalculator.getMinValue(arr);
+        assertEquals(-3, minValue);
+    }
+
 }
