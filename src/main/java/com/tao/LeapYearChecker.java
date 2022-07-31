18be97836192d5206a385dc4763b109afeadd57d
taofiqSulayman <taofiqsulayman@gmail.com>
2022-07-31 18:45:09 +0100

    Refactored code to remove duplicate "ifs"

--- src/main/java/com/tao/LeapYearChecker.java
+++ src/main/java/com/tao/LeapYearChecker.java
@@ -5,9 +5,6 @@ public class LeapYearChecker {
 
         if (year % 100 == 0 && year % 400 != 0)
             return false;
-
-        if (year % 4 == 0)
-            return true;
-        return false;
+        return year % 4 == 0 || year % 400 == 0;
     }
 }
