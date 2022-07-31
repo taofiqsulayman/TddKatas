e81c092796b9983c662ff64e4749f1aede518e00
taofiqSulayman <taofiqsulayman@gmail.com>
2022-07-31 21:32:21 +0100

    obvious implementation

--- src/main/java/com/tao/RomanNumeralsConverter.java
+++ src/main/java/com/tao/RomanNumeralsConverter.java
@@ -1,7 +1,10 @@
 package com.tao;
 
 public class RomanNumeralsConverter {
-    public static String toRoman(int i) {
+    public static String toRoman(int number) {
+        if (number == 2)
+            return "II";
+
         return "I";
     }
 }
