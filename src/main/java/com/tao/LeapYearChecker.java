486760eae174e59e23d55f13421fe8a9576120e0
taofiqSulayman <taofiqsulayman@gmail.com>
2022-07-31 18:16:18 +0100

    faked it to pass the 3rd test

--- src/main/java/com/tao/LeapYearChecker.java
+++ src/main/java/com/tao/LeapYearChecker.java
@@ -2,6 +2,9 @@ package com.tao;
 
 public class LeapYearChecker {
     public static boolean isLeapYear(int year) {
+        if (year % 100 == 0)
+            return false;
+
         if (year % 4 == 0)
             return true;
         return false;
